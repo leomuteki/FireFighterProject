7a2ee67f12b1f7a8244835f5f55eb6d797828d5e
dzhan008 <dzhan008@ucr.edu>
2018-04-23 21:30:53 -0700

    Added Smoke Test/Camera on FLIR
    -Added a new scene testing smoke effects in Unity via particle effects
    -Created a render texture on the FLIR so now the device can see another view through the screen
    -Created a simple test shader to test color displacements for the FLIR view

---
+++ Assets/Scripts/CustomImageEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomImageEffect : MonoBehaviour {
+
+    public Material EffectMaterial;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, EffectMaterial);
+    }
+}
---
+++ Assets/Scripts/ParticleTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour {
+    public ParticleSystem ps;
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+    // Use this for initialization
+    void Start () {
+        ps = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleTrigger()
+    {
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+
+        for(int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            Color color = p.startColor;
+            p.startColor = new Color(255, 0, 0, 0);
+            enter[i] = p;
+            /*while (p.startColor.a > 0)
+            {
+                p.startColor = new Color(color.r, color.g, color.b, color.a - (Time.deltaTime));
+                enter[i] = p;
+                //ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+                Debug.Log(p.startColor.a);
+            }*/
+        }
+
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+    }
+}
