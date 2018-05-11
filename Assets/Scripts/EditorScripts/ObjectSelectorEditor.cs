fa5bfca0b43b145fa334152a408359931ba0dc59
Emmilio <esego001@ucr.edu>
2018-05-10 22:17:54 -0700

    Made DoubleMeshScene

---
+++ Assets/Scripts/EditorScripts/ObjectSelectorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ObjectSelectorScript))]
+public class ObjectSelectorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ObjectSelectorScript myScript = (ObjectSelectorScript)target;
+        if (GUILayout.Button("Select Objects"))
+        {
+            myScript.SelectObjects();
+        }
+    }
+}
---
+++ Assets/Scripts/EditorScripts/ObjectSelectorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSelectorScript : MonoBehaviour
+{
+    public Material Material1;
+
+
+    public void SelectObjects()
+    {
+        object[] obj = GameObject.FindSceneObjectsOfType(typeof(GameObject));
+        foreach (object o in obj)
+        {
+            GameObject g = (GameObject)o;
+            if (g.GetComponent<MeshRenderer>())
+            {
+                g.GetComponent<MeshRenderer>().material = Material1;
+            }
+        }
+    }
+}
