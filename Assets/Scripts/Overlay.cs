067690e7e033fe7c8df961ab096ee037691c6dd9
dzhan008 <dzhan008@ucr.edu>
2018-05-11 12:34:42 -0700

    Added simple overlay
    -For now, it just switches between the culling masks Emmilio created.
    -Also added a new layer for the FLIR since the FLIR needs to be shown in both views.

---
+++ Assets/Scripts/Overlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Overlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GetComponent<Camera>().cullingMask = ~(1 << LayerMask.NameToLayer("ThermalLayer"));
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GetComponent<Camera>().cullingMask = (1 << LayerMask.NameToLayer("ThermalLayer")) | (1 << LayerMask.NameToLayer("FireLayer")) | (1 << LayerMask.NameToLayer("FLIRLayer"));
+        }
+		
+	}
+}
